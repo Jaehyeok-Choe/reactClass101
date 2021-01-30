32271f8ccb909b535093c7733f90a854d6c77333
tbeben77 <tbeben77@gmail.com>
2021-01-31 00:54:46 +0900

    useState 사용법 연습

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
+/* eslint-disable*/
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  
+
+  let [title, changeTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '울산 대방어 맛집']);
+  let [like, countUpLike] = useState(0);
+  let posts = '강남 고기 맛집';
+
+  function functionChangeTitle(){
+    var newArray = [...title];
+    newArray[0] = '여자 코트 추천';
+    changeTitle(newArray);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          리액트 깃허브 커밋 테스트
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      <button onClick={ functionChangeTitle}>Button</button>
+      <div className="list">
+        <h3>{title[0]}<span onClick={ ()=>{countUpLike(like + 1)}}>👍</span> {like} </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{title[1]}</h3>
+        <p>3월 17일 발행</p>
+        <hr/>
+      </div>   
+      <div className="list">
+        <h3>{title[2]}</h3>
+        <p>5월 17일 발행</p>
+        <hr/>
+      </div>         
     </div>
   );
 }
