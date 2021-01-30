5545b30779986ea7c8ddf820e2604ce89c3f6f3a
tbeben77 <tbeben77@gmail.com>
2021-01-30 23:39:33 +0900

    커밋테스트

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          안녕하세요
+          리액트 깃허브 커밋 테스트
         </p>
         <a
           className="App-link"
